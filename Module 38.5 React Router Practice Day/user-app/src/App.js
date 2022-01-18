10206832d844a7f315bafecaf08ccea9a6a7dd2e
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-18 20:57:41 +0600

    creating some nav element

--- Module 38.5 React Router Practice Day/user-app/src/App.js
+++ Module 38.5 React Router Practice Day/user-app/src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/Header/Header";
 import Users from "./components/Users/Users";
 
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Users></Users>
     </div>
   );
