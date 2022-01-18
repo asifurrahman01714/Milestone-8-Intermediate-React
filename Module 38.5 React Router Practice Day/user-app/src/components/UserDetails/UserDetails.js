1b7b0790049ae9f67e5613c2157295715274eb35
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-18 22:18:11 +0600

    see user Details compo completed

--- Module 38.5 React Router Practice Day/user-app/src/components/UserDetails/UserDetails.js
+++ Module 38.5 React Router Practice Day/user-app/src/components/UserDetails/UserDetails.js
@@ -10,11 +10,23 @@ const UserDetails = () => {
         .then(res => res.json())
         .then(data => setUsers(data))
     },[]);
-    const singleUser = users.find(user => user.id === parseInt(id)); //user was not showign at first. so you have to convert the string to integer
-    console.log(singleUser);
+    const user = users.find(user => user.id === parseInt(id)); //user was not showign at first. so you have to convert the string to integer
+    // console.log(user);
+    const {name, email} = user;
+    const imageUrl = 'https://cn.i.cdn.ti-platform.com/cnapac/content/2017/showpage/ben-10/sa/showicon.png';
     return (
-        <div>
-            <h1>User Details of : {id}</h1>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className='col-md-4 '>
+                    <div className="card m-2" style={{width: '18rem'}}>
+                        <img src={imageUrl} className="card-img-top p-2" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title"><strong>Name:</strong>{name}</h5>
+                            <h6 className="card-title"><strong>Email:</strong> {email}</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
