2f4168af567ee6eddaef8bb8e693fab0cd6580a9
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-18 20:50:05 +0600

    show data from props

--- Module 38.5 React Router Practice Day/user-app/src/components/User/User.js
+++ Module 38.5 React Router Practice Day/user-app/src/components/User/User.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const User = () => {
+const User = ({user}) => {
+    const {name, email} = user;
     return (
         <div>
-            <h1>User</h1>
+            <h1>User Name: {name}</h1>
+            <h1>User Email: {email}</h1>
         </div>
     );
 };
