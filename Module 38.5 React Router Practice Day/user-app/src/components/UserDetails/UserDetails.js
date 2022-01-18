9116b45cc1e89a0406d7ff35e4c745d73147541e
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-18 22:03:30 +0600

    useState

--- Module 38.5 React Router Practice Day/user-app/src/components/UserDetails/UserDetails.js
+++ Module 38.5 React Router Practice Day/user-app/src/components/UserDetails/UserDetails.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const UserDetails = () => {
+    const {id} = useParams();
+    const [user, setUser] = React.userState([]);
+    
     return (
         <div>
-            <h1>User Details</h1>
+            <h1>User Details of : {id}</h1>
         </div>
     );
 };
