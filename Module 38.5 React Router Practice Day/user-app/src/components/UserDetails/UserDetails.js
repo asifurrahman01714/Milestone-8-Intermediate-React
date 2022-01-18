68778e9c18bc7ce1948da14a353646dc41cc4e57
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-18 22:10:47 +0600

    user was not showign at first. so you have to convert the string to integer

--- Module 38.5 React Router Practice Day/user-app/src/components/UserDetails/UserDetails.js
+++ Module 38.5 React Router Practice Day/user-app/src/components/UserDetails/UserDetails.js
@@ -3,14 +3,15 @@ import { useParams } from 'react-router-dom';
 
 const UserDetails = () => {
     const {id} = useParams();
+    console.log(id);
     const [users, setUsers] = React.useState([]);
     React.useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(res => res.json())
         .then(data => setUsers(data))
     },[]);
-    const user = users.find(user => user.id === id);
-    console.log(user);
+    const singleUser = users.find(user => user.id === parseInt(id));
+    console.log(singleUser);
     return (
         <div>
             <h1>User Details of : {id}</h1>
