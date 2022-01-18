ff22929aef1ccc8fa89702812bd1543e57ebe030
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-18 22:04:12 +0600

    useEffect and set data

--- Module 38.5 React Router Practice Day/user-app/src/components/UserDetails/UserDetails.js
+++ Module 38.5 React Router Practice Day/user-app/src/components/UserDetails/UserDetails.js
@@ -4,7 +4,11 @@ import { useParams } from 'react-router-dom';
 const UserDetails = () => {
     const {id} = useParams();
     const [user, setUser] = React.userState([]);
-    
+    React.useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setUser(data))
+    },[])
     return (
         <div>
             <h1>User Details of : {id}</h1>
