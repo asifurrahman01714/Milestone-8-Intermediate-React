ac7d276fb8ea7b001f0680b78cfe335af75d1796
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-18 22:33:22 +0600

    bootstrap img w-50 rounded mx-auto d-block img-thumbnail

--- Module 38.5 React Router Practice Day/user-app/src/components/UserDetails/UserDetails.js
+++ Module 38.5 React Router Practice Day/user-app/src/components/UserDetails/UserDetails.js
@@ -11,18 +11,21 @@ const UserDetails = () => {
         .then(data => setUsers(data))
     },[]);
     const user = users.find(user => user.id === parseInt(id)); //user was not showign at first. so you have to convert the string to integer
-    // console.log(user);
-    const {name, email} = user;
+    console.log(users);
+    console.log(user);
+    // const {name, email} = user;
     const imageUrl = 'https://cn.i.cdn.ti-platform.com/cnapac/content/2017/showpage/ben-10/sa/showicon.png';
     return (
         <div className="container">
             <div className="row justify-content-center">
-                <div className='col-md-4 '>
-                    <div className="card m-2" style={{width: '18rem'}}>
-                        <img src={imageUrl} className="card-img-top p-2" alt="..."/>
+                <div className='col-md-6 '>
+                    <div className="card m-2">
+                        <img src={imageUrl} className="card-img-top p-5 w-50 rounded mx-auto d-block img-thumbnail" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title"><strong>Name:</strong>{name}</h5>
-                            <h6 className="card-title"><strong>Email:</strong> {email}</h6>
+                            <h5 className="card-title"><strong>Name:</strong>{user?.name}</h5>
+                            <h5 className="card-title"><strong>Phone:</strong>{user?.phone}</h5>
+                            <h5 className="card-title"><strong>Website:</strong>{user?.website}</h5>
+                            <h6 className="card-title"><strong>Email:</strong> {user?.email}</h6>
                         </div>
                     </div>
                 </div>
