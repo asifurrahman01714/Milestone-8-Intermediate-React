1d1b55b0929a5e7aca5a2819bac0ea2d0fa4a81a
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-19 15:00:11 +0600

    calling data in another way

--- Module 38.5 React Router Practice Day/user-app/src/components/UserDetails/UserDetails.js
+++ Module 38.5 React Router Practice Day/user-app/src/components/UserDetails/UserDetails.js
@@ -4,16 +4,21 @@ import { useParams } from 'react-router-dom';
 const UserDetails = () => {
     const {id} = useParams();
     console.log(id);
-    const [users, setUsers] = React.useState([]);
-    React.useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then(data => setUsers(data))
+    const [user, setUser] = React.useState({});
+    React.useEffect(()=>{
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then(response => response.json())
+        .then(data => setUser(data))
     },[]);
-    const user = users.find(user => user.id === parseInt(id)); //user was not showign at first. so you have to convert the string to integer
-    console.log(users);
+    // React.useEffect(() => {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(res => res.json())
+    //     .then(data => setUsers(data))
+    // },[]);
+    // const user = users.find(user => user.id === parseInt(id)); //user was not showign at first. so you have to convert the string to integer
+    // console.log(users);
     console.log(user);
-    // const {name, email} = user;
+    const {name, email, phone, website} = user;
     const imageUrl = 'https://cn.i.cdn.ti-platform.com/cnapac/content/2017/showpage/ben-10/sa/showicon.png';
     return (
         <div className="container mt-4">
@@ -22,10 +27,10 @@ const UserDetails = () => {
                     <div className="card m-2">
                         <img src={imageUrl} className="card-img-top p-5 w-50 mt-2 rounded mx-auto d-block img-thumbnail" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title"><strong>Name: </strong>{user?.name}</h5>
-                            <h5 className="card-title"><strong>Phone: </strong>{user?.phone}</h5>
-                            <h5 className="card-title"><strong>Website: </strong>{user?.website}</h5>
-                            <h6 className="card-title"><strong>Email: </strong> {user?.email}</h6>
+                            <h5 className="card-title"><strong>Name: </strong>{name}</h5>
+                            <h5 className="card-title"><strong>Phone: </strong>{phone}</h5>
+                            <h5 className="card-title"><strong>Website: </strong>{website}</h5>
+                            <h6 className="card-title"><strong>Email: </strong> {email}</h6>
                         </div>
                     </div>
                 </div>
