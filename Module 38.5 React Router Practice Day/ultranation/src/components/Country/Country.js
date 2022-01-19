32036529d5b8fce948acea86663b90907a85b9bc
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-19 17:32:48 +0600

    useHistory

--- Module 38.5 React Router Practice Day/ultranation/src/components/Country/Country.js
+++ Module 38.5 React Router Practice Day/ultranation/src/components/Country/Country.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Country = (props) => {
     const name = props.country.name?.common;
+    const history = useHistory();
+    const handleClick = (name) => {
+        history.push(`/country/${name}`);
+    }
     return (
         <div className="col-md-4">
             <div className="card mt-3 shadow">
                 <img src={props.country.flags.png} className="card-img-top" style={{height: '200px'}} alt="..."/>
                 <div className="card-body">
                     <h6 className="card-title text-center fw-bold">{name}</h6>
-                    <Link href="#" className="mt-3 btn btn-primary d-flex justify-content-center" to={`/country/${name}`}>See details</Link>
+                    {/* <Link className="mt-3 btn btn-primary d-flex justify-content-center" to={`/country/${name}`}>See details</Link> */}
+                    <div className="d-flex justify-content-center">
+                        <button className="mt-3 btn btn-primary " onClick={()=>handleClick(name)}>See Details</button>
+                    </div>
                 </div>
             </div>
         </div>
