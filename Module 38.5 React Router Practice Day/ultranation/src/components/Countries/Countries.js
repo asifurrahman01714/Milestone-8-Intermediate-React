1321d42650bceccceca2b489083bb201f39d59fd
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-19 18:26:56 +0600

    showing spinner in react

--- Module 38.5 React Router Practice Day/ultranation/src/components/Countries/Countries.js
+++ Module 38.5 React Router Practice Day/ultranation/src/components/Countries/Countries.js
@@ -3,29 +3,33 @@ import Country from '../Country/Country';
 
 const Countries = () => {
     const [countries, setCountries] = React.useState([]);
+    const [spinner ,setSpinner] = React.useState(true);
     React.useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
             .then(data => {
                 setCountries(data);
                 console.log(data);
+                setSpinner(false);
             });
     },[])
     return (
         <div className="container">
             
             {
-                countries.length === 250 ? 
-                <div className="row">
-                    {
-                        countries.map(country => <Country key={country.name?.common} country={country}></Country>)
-                    }
-                </div> :
+                spinner === true ? 
                 <div className="row justify-content-center mt-5">
                     <div class="spinner-border text-danger" role="status">
                         <span class="visually-hidden"></span>
                     </div>
                 </div>
+                :
+                
+                <div className="row">
+                    {
+                        countries.map(country => <Country key={country.name?.common} country={country}></Country>)
+                    }
+                </div> 
             }
         </div>
     );
