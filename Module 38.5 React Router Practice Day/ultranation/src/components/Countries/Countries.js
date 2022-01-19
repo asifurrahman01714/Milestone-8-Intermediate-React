14102f41fa0a5875409494c261afa14585ce5849
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-19 18:24:20 +0600

    bootstrap spinner in react

--- Module 38.5 React Router Practice Day/ultranation/src/components/Countries/Countries.js
+++ Module 38.5 React Router Practice Day/ultranation/src/components/Countries/Countries.js
@@ -13,11 +13,20 @@ const Countries = () => {
     },[])
     return (
         <div className="container">
-            <div className="row">
-                {
-                    countries.map(country => <Country key={country.name?.common} country={country}></Country>)
-                }
-            </div>
+            
+            {
+                countries.length === 250 ? 
+                <div className="row">
+                    {
+                        countries.map(country => <Country key={country.name?.common} country={country}></Country>)
+                    }
+                </div> :
+                <div className="row justify-content-center mt-5">
+                    <div class="spinner-border text-danger" role="status">
+                        <span class="visually-hidden"></span>
+                    </div>
+                </div>
+            }
         </div>
     );
 };
