46a475eba832ce1ed167103fe0dd896d2e528570
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-19 17:03:10 +0600

    useparams

--- Module 38.5 React Router Practice Day/ultranation/src/components/CountryDetails/CountryDetails.js
+++ Module 38.5 React Router Practice Day/ultranation/src/components/CountryDetails/CountryDetails.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const CountryDetails = () => {
+    const {name} = useParams();
+    // console.log(useParams());
     return (
         <div>
             <h1>CountryDetails</h1>
