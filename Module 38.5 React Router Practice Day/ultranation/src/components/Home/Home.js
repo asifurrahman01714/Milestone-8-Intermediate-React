f21e877a85936397485ab7c1b5eead54a6d16a97
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-19 16:29:18 +0600

    countries compo

--- Module 38.5 React Router Practice Day/ultranation/src/components/Home/Home.js
+++ Module 38.5 React Router Practice Day/ultranation/src/components/Home/Home.js
@@ -12,11 +12,11 @@ const Home = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" href="/home">Home</Link>
+                        <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
                     </li>
                     
                     <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" href="#">All Countries</Link>
+                        <Link className="nav-link active" aria-current="page" to="/home">All Countries</Link>
                     </li>
                 </ul>
                 <form className="d-flex">
