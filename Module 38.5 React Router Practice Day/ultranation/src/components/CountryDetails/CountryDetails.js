ceb60fe084867196f3a692981c8122719e16c0b6
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-19 18:34:08 +0600

    spinner in react

--- Module 38.5 React Router Practice Day/ultranation/src/components/CountryDetails/CountryDetails.js
+++ Module 38.5 React Router Practice Day/ultranation/src/components/CountryDetails/CountryDetails.js
@@ -6,11 +6,13 @@ const CountryDetails = () => {
     // console.log(useParams());
     const url = `https://restcountries.com/v3.1/name/${name}`;
     const [country, setCountry] = React.useState({});
+    const [spinner, setSpinner] = React.useState(true);
     React.useEffect(() => {
         fetch(url)
             .then(res => res.json())
             .then(data => {
                 setCountry(data[0]);
+                setSpinner(false);
             });
     },[])
     console.log(country);
@@ -22,17 +24,25 @@ const CountryDetails = () => {
     return (
         <div className="container">
             <div className="row justify-content-center mt-3">
-                <div className="col-6">
-                    <div class="card shadow">
-                    <img src={image} class="card-img-top" alt="..."/>
-                    <div class="card-body">
-                        <h5 class="card-title">Name: {name}</h5>
-                        <h5 class="card-title">area: {area}</h5>
-                        <h5 class="card-title">Population: {population}</h5>
-                        <h5 class="card-title text-success">Name: {status}</h5>
+                {
+                    spinner === true ?
+                    <div class="spinner-border text-danger" role="status">
+                            <span class="visually-hidden"></span>
                     </div>
-                </div>
-                </div>
+                    :
+                    <div className="col-6">
+                        <div class="card shadow">
+                            <img src={image} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Name: {name}</h5>
+                                <h5 class="card-title">area: {area}</h5>
+                                <h5 class="card-title">Population: {population}</h5>
+                                <h5 class="card-title text-success">Name: {status}</h5>
+                            </div>
+                        </div>
+                    </div>
+                }
+                
             </div>
         </div>
     );
