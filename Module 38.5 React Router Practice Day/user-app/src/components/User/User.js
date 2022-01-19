00248714c71a02be84c1e145fd4c56df7c81f49c
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-19 15:22:26 +0600

    useHistory in React

--- Module 38.5 React Router Practice Day/user-app/src/components/User/User.js
+++ Module 38.5 React Router Practice Day/user-app/src/components/User/User.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const User = ({user, userDetails}) => {
+    const history = useHistory();
     const {name, email, id} = user;
+    const handleClick = () => {
+        history.push(`/userDetails/${id}`);
+    }
+    
     const imageUrl = 'https://cn.i.cdn.ti-platform.com/cnapac/content/2017/showpage/ben-10/sa/showicon.png';
     return (
         <div className='col-md-4 '>
@@ -11,7 +16,8 @@ const User = ({user, userDetails}) => {
                 <div className="card-body">
                     <h5 className="card-title"><strong>Name:</strong>{name}</h5>
                     <h6 className="card-title"><strong>Email:</strong> {email}</h6>
-                    <Link className="btn btn-primary d-flex justify-content-center mt-4"  to={`/userDetails/${id}`} onClick={()=>userDetails(user)}>See Details</Link>
+                    {/* <Link className="btn btn-primary d-flex justify-content-center mt-4"  to={`/userDetails/${id}`} >See Details</Link> */}
+                    <Link className="btn btn-primary d-flex justify-content-center mt-4"  onClick={()=> handleClick(id)} >See Details</Link>
                 </div>
             </div>
         </div>
