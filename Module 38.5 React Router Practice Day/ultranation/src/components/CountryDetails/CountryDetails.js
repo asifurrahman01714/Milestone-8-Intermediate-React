89acb2dfdbce7808239d3fc53bb9c9927395b8e8
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-19 17:20:06 +0600

    Omg..fo toUpperCase we have to use optional chaining

--- Module 38.5 React Router Practice Day/ultranation/src/components/CountryDetails/CountryDetails.js
+++ Module 38.5 React Router Practice Day/ultranation/src/components/CountryDetails/CountryDetails.js
@@ -4,9 +4,36 @@ import { useParams } from 'react-router-dom';
 const CountryDetails = () => {
     const {name} = useParams();
     // console.log(useParams());
+    const url = `https://restcountries.com/v3.1/name/${name}`;
+    const [country, setCountry] = React.useState({});
+    React.useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setCountry(data[0]);
+            });
+    },[])
+    console.log(country);
+    const image = country.flags?.png;
+    const area = country?.area;
+    const population = country?.population;
+    let status = country?.status;
+    status = status?.toUpperCase(); // Omg..fo toUpperCase we have to use optional chaining
     return (
-        <div>
-            <h1>CountryDetails</h1>
+        <div className="container">
+            <div className="row justify-content-center mt-3">
+                <div className="col-6">
+                    <div class="card">
+                    <img src={image} class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Name: {name}</h5>
+                        <h5 class="card-title">area: {area}</h5>
+                        <h5 class="card-title">Name: {population}</h5>
+                        <h5 class="card-title text-success">Name: {status}</h5>
+                    </div>
+                </div>
+                </div>
+            </div>
         </div>
     );
 };
