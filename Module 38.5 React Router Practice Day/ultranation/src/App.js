a152f1762eaf8599807ec741c20af58ceeaa186e
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-19 16:12:05 +0600

    hmm..user Link after declaration react router

--- Module 38.5 React Router Practice Day/ultranation/src/App.js
+++ Module 38.5 React Router Practice Day/ultranation/src/App.js
@@ -1,9 +1,15 @@
-
+import Home from "./components/Home/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Home></Home>
+    </Router>
   );
 }
 
--- Module 38.5 React Router Practice Day/ultranation/src/components/Home/Home.js
+++ Module 38.5 React Router Practice Day/ultranation/src/components/Home/Home.js
@@ -1,22 +1,22 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand fw-bold text-primary" href="#">See All Country</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <Link className="nav-link active" aria-current="page" href="/home">Home</Link>
                     </li>
                     
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">All Countries</a>
+                        <Link className="nav-link active" aria-current="page" href="#">All Countries</Link>
                     </li>
                 </ul>
                 <form className="d-flex">
