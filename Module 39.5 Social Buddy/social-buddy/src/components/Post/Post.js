1ad756e4dd4f8971c0661c64348fae565cf4c8a9
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-24 13:32:35 +0600

    something wrong

--- Module 39.5 Social Buddy/social-buddy/src/components/Post/Post.js
+++ Module 39.5 Social Buddy/social-buddy/src/components/Post/Post.js
@@ -4,11 +4,10 @@ const Post = (props) => {
     const {name, body} = props.post;
     return (
         <div className='col-md-4'>
-            <div class="card mt-4 shadow" >
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className="card mt-4 shadow" >
+                <div className="card-body">
+                    <h1 className="card-title fw-bold ">{name}</h1>
+                    <p className="card-text">{body}</p>
                 </div>
             </div>
         </div>
