25e417ebf0b0381fc09e744e4abd42b6b090eabf
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-24 13:38:36 +0600

    adding css

--- Module 39.5 Social Buddy/social-buddy/src/components/Post/Post.js
+++ Module 39.5 Social Buddy/social-buddy/src/components/Post/Post.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import './post.css';
 const Post = (props) => {
-    const {name, body} = props.post;
+    const {title, body} = props.post;
     return (
         <div className='col-md-4'>
-            <div className="card mt-4 shadow" >
+            <div className="card mt-4 shadow" style={{height:'250px'}}>
                 <div className="card-body">
-                    <h1 className="card-title fw-bold ">{name}</h1>
-                    <p className="card-text">{body}</p>
+                    <h5 className="card-title fw-bold">{title}</h5>
+                    <p className="card-text" >{body}</p>
                 </div>
             </div>
         </div>
