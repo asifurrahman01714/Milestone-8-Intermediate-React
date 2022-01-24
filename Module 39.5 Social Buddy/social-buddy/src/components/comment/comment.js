f9de17165fc8177551b3240723e8bbca51493a77
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-24 15:19:45 +0600

    creating comment compo

---
+++ Module 39.5 Social Buddy/social-buddy/src/components/comment/comment.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const comment = () => {
+    return (
+        <div className='col-md-4'>
+            <div className="card mt-4 shadow" style={{height:'250px'}}>
+                <div className="card-body text-center">
+                    <h5 className="card-title fw-bold">sdfadsf</h5>
+                    <p className="card-text" >asdfsdf</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default comment;
