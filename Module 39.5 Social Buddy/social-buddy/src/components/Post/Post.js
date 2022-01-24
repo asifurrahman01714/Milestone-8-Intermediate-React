0b06166c0f6539670b985088e38dbecc8ad4b7cc
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-24 22:18:46 +0600

    erase everything

--- Module 39.5 Social Buddy/social-buddy/src/components/Post/Post.js
+++ Module 39.5 Social Buddy/social-buddy/src/components/Post/Post.js
@@ -1,26 +1,13 @@
 import { Button } from '@mui/material';
-import React, { useContext } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { SearchContext } from '../../App';
 import './post.css';
 const Post = (props) => {
     const {title, body, id} = props.post;
     const history = useHistory();
     const handleClick = () => {
         history.push(`/comments/${id}`);
-    };
-    const [search, setSearch] = useContext(SearchContext);
-    const [post, setPost] = React.useState([]);
-    
-    React.useEffect(()=>{
-        const url = `https://jsonplaceholder.typicode.com/posts?id=${search}`;
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            setPost(data);
-        });
-    }, []);
-    console.log(post);
+    }
     return (
         <div className='col-md-4'>
             <div className="card mt-4 shadow" style={{height:'250px'}}>
