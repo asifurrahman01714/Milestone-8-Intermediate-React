3bae7e8aea0651adce0f7887e4992af97d0da0a2
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-24 15:02:01 +0600

    text-center

--- Module 39.5 Social Buddy/social-buddy/src/components/Post/Post.js
+++ Module 39.5 Social Buddy/social-buddy/src/components/Post/Post.js
@@ -11,7 +11,7 @@ const Post = (props) => {
     return (
         <div className='col-md-4'>
             <div className="card mt-4 shadow" style={{height:'250px'}}>
-                <div className="card-body">
+                <div className="card-body text-center">
                     <h5 className="card-title fw-bold">{title}</h5>
                     <p className="card-text" >{body}</p>
                     <Button variant="contained" onClick={handleClick}>Contained</Button>
