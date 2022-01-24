71c1d878aa1f61ccd6285ff06ebab67d2e598a98
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-24 18:27:00 +0600

    bootstrap spinner in react

--- Module 39.5 Social Buddy/social-buddy/src/components/Comments/Comments.js
+++ Module 39.5 Social Buddy/social-buddy/src/components/Comments/Comments.js
@@ -5,11 +5,15 @@ import Comment from '../Comment/Comment';
 const Comments = () => {
     const {postId} = useParams();
     const [comments, setComments] = React.useState([]);
+    const [spinner ,setSpinner] = React.useState(true);
     React.useEffect(()=>{
         const url = `https://jsonplaceholder.typicode.com/comments?postId=${postId}`;
         fetch(url)
         .then(response => response.json())
-        .then(data => setComments(data));
+        .then(data => {
+            setComments(data);
+            setSpinner(false);
+        });
     },[])
     /*
     React.useEffect(()=>{
@@ -23,11 +27,21 @@ const Comments = () => {
 
     return (
         <div className="container">
-            <div className="row">
-                {
-                    comments.map(comment => <Comment key={comment.id} comment={comment}/>)
-                }
-            </div>
+            {
+                spinner === true ? 
+                <div className="row justify-content-center mt-5">
+                    <div class="spinner-border text-danger" role="status">
+                        <span class="visually-hidden"></span>
+                    </div>
+                </div>
+                :
+
+                <div className="row">
+                    {
+                        comments.map(comment => <Comment key={comment.id} comment={comment}/>)
+                    }
+                </div>
+            }
         </div>
     );
 };
