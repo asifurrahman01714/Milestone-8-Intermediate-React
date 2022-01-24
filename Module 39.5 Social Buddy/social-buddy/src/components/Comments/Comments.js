eeec0c920ae314a48a4afbf101614f197830e554
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-24 15:18:28 +0600

    filtering comments

--- Module 39.5 Social Buddy/social-buddy/src/components/Comments/Comments.js
+++ Module 39.5 Social Buddy/social-buddy/src/components/Comments/Comments.js
@@ -4,9 +4,16 @@ import { useParams } from 'react-router-dom';
 const Comments = () => {
     const {postId} = useParams();
     const [comments, setComments] = React.useState([]);
+    React.useEffect(()=>{
+        const url = `https://jsonplaceholder.typicode.com/comments`;
+        fetch(url)
+        .then(response => response.json())
+        .then(data => setComments(data));
+    }, []);
+    const filteredComments = comments.filter(comment => comment.postId === parseInt(postId));
     return (
         <div>
-            <h1>Comments of {postId}</h1>
+            <h1>Comments of {postId} is {filteredComments.length}</h1>
         </div>
     );
 };
