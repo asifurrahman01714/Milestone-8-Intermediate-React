2e936479e09e26900059a008d7bb8965c25a9ac3
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-24 18:44:11 +0600

    completed material UI card

--- Module 39.5 Social Buddy/social-buddy/src/components/comment/comment.js
+++ Module 39.5 Social Buddy/social-buddy/src/components/comment/comment.js
@@ -4,32 +4,25 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
-
 const Comment = ({comment}) => {
     const {name, email, body} = comment;
     return (
         <div className='col-md-4'>
             <Card sx={{ maxWidth: 345 }}>
                 <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    height="140"
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    alt="green iguana"
-                    />
                     <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
-                    </Typography>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {name}
+                        </Typography>
+                        <h6 className="">email: {email}</h6>
+                        <Typography variant="body2" color="text.secondary">
+                            {body}
+                        </Typography>
                     </CardContent>
                 </CardActionArea>
                 <CardActions>
-                    <Button size="small" color="primary">
-                    Share
+                    <Button variant="contained" size="small" color="primary">
+                        Share
                     </Button>
                 </CardActions>
             </Card>
