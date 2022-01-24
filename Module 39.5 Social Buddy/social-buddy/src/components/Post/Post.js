61619b4363cd83f9f5560fdf6d0e60adf3131be5
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-24 15:00:14 +0600

    set history

--- Module 39.5 Social Buddy/social-buddy/src/components/Post/Post.js
+++ Module 39.5 Social Buddy/social-buddy/src/components/Post/Post.js
@@ -1,15 +1,20 @@
 import { Button } from '@mui/material';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './post.css';
 const Post = (props) => {
     const {title, body} = props.post;
+    const history = useHistory();
+    const handleClick = () => {
+        history.push(`/post/${props.post.id}`);
+    }
     return (
         <div className='col-md-4'>
             <div className="card mt-4 shadow" style={{height:'250px'}}>
                 <div className="card-body">
                     <h5 className="card-title fw-bold">{title}</h5>
                     <p className="card-text" >{body}</p>
-                    <Button variant="contained">Contained</Button>
+                    <Button variant="contained" onClick={handleClick}>Contained</Button>
                 </div>
             </div>
         </div>
