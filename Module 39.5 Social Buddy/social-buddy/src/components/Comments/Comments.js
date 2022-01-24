b3073924124558a699359450f80b07490509f3e9
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-24 15:12:01 +0600

    useState

--- Module 39.5 Social Buddy/social-buddy/src/components/Comments/Comments.js
+++ Module 39.5 Social Buddy/social-buddy/src/components/Comments/Comments.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 
 const Comments = () => {
     const {postId} = useParams();
+    const [comments, setComments] = React.useState([]);
     return (
         <div>
             <h1>Comments of {postId}</h1>
