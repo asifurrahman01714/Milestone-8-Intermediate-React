1df59240cd6a16fe504b3ef2f1e6f24bc7a8909e
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-24 18:44:30 +0600

    neglecting bootstrap card

--- Module 39.5 Social Buddy/social-buddy/src/components/comment/comment.js
+++ Module 39.5 Social Buddy/social-buddy/src/components/comment/comment.js
@@ -26,13 +26,13 @@ const Comment = ({comment}) => {
                     </Button>
                 </CardActions>
             </Card>
-            <div className="card mt-4 shadow" style={{height:'250px'}}>
+            {/* <div className="card mt-4 shadow" style={{height:'250px'}}>
                 <div className="card-body text-center">
                     <h5 className="card-title fw-bold">{name}</h5>
                     <h6 className="">email: {email}</h6>
                     <p className="mt-3 pt-2 border-primary border-top card-text" >{body}</p>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
