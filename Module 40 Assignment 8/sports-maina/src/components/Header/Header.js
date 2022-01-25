e608151d66383f8308d66f213a408ce708b7b1cd
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-25 17:26:21 +0600

    navbar-dark bg-dark

--- Module 40 Assignment 8/sports-maina/src/components/Header/Header.js
+++ Module 40 Assignment 8/sports-maina/src/components/Header/Header.js
@@ -7,9 +7,9 @@ const Header = () => {
             <div style={{height: '200px',alignItems: 'center',justifyContent: 'center',display: 'flex'}}>
             <img style={{width: '70%',height: '100%',margin: 'auto'}} className="img-fluid" src="https://play-lh.googleusercontent.com/DEX2nfpOsE6b8rWD5scwkelM9Cnoapqm-O8L6zb5vr82CgGCFB4kZz-bUbcjZ09HWg=w412-h220-rw" alt="" />
         </div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark mt-2">
             <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand fw-b" href="#">Sports Mania</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
--- Module 40 Assignment 8/sports-maina/src/components/Header/dist/Header.dev.js
+++ Module 40 Assignment 8/sports-maina/src/components/Header/dist/Header.dev.js
@@ -1,17 +1,1 @@
 "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-
-var _react = _interopRequireDefault(require("react"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var Header = function Header() {
-  return div;
-};
-
-var _default = Header;
-exports["default"] = _default;
