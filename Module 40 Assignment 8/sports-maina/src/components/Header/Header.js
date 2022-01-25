d807832f6b8d63ab38dba132709dae2c5c606c3b
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-25 17:22:13 +0600

    bootstrap navbar in react

--- Module 40 Assignment 8/sports-maina/src/components/Header/Header.js
+++ Module 40 Assignment 8/sports-maina/src/components/Header/Header.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div style={{height: '200px',alignItems: 'center',justifyContent: 'center',display: 'flex'}}>
+        <div>
+            <div style={{height: '200px',alignItems: 'center',justifyContent: 'center',display: 'flex'}}>
             <img style={{width: '70%',height: '100%',margin: 'auto'}} className="img-fluid" src="https://play-lh.googleusercontent.com/DEX2nfpOsE6b8rWD5scwkelM9Cnoapqm-O8L6zb5vr82CgGCFB4kZz-bUbcjZ09HWg=w412-h220-rw" alt="" />
+        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+                </li>
+                </ul>
+                <form className="d-flex">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+            </div>
+        </nav>
         </div>
     );
 };
--- Module 40 Assignment 8/sports-maina/src/components/Header/dist/Header.dev.js
+++ Module 40 Assignment 8/sports-maina/src/components/Header/dist/Header.dev.js
@@ -1,3 +1,17 @@
 "use strict";
 
-rsc;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var Header = function Header() {
+  return div;
+};
+
+var _default = Header;
+exports["default"] = _default;
