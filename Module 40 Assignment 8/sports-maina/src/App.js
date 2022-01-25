33436634c85d5021dacef027d857c22d25b2df68
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-25 17:06:33 +0600

    adding header

--- Module 40 Assignment 8/sports-maina/src/App.js
+++ Module 40 Assignment 8/sports-maina/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [search, setSearch] = useState("")
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
