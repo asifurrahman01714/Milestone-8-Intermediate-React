46dbfc3404c60f929421f37a5d1da1bb08dd01b9
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-25 16:49:49 +0600

    erase everything

--- Module 40 Assignment 8/sports-maina/src/App.js
+++ Module 40 Assignment 8/sports-maina/src/App.js
@@ -1,30 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import Data from './FakeData/FakeData.json';
-
 
 function App() {
   const [search, setSearch] = useState("")
   return (
     <div className="App">
-      <div>
-      <input placeholder="Enter Post Title" onChange={event => setSearch(event.target.value)} />
-      <h1>Written terms: {search}</h1>
-      {
-        Data.filter(post => {
-          if (search === '') {
-            return post;
-          } else if (post.title.toLowerCase().includes(search.toLowerCase())) {
-            return post;
-          }
-        }).map((post, index) => (
-          <div className="box" key={index}>
-            <p>{post.title}</p>
-            <p>{post.author}</p>
-          </div>
-        ))
-      }
-      </div>
+      
     </div>
   );
 }
