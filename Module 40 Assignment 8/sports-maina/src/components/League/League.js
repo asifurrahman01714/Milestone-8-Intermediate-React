b0ea406ad37d81de44c9de274f82bee1b692c779
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-25 20:39:05 +0600

    change something

--- Module 40 Assignment 8/sports-maina/src/components/League/League.js
+++ Module 40 Assignment 8/sports-maina/src/components/League/League.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const League = ({league}) => {
-    const {idLeague,strLeague,strCountry,strFanart1,strDescriptionEN} = league;
+    const {idLeague,strLeague,strCountry,strFanart1,strFanart2,strDescriptionEN,strBanner,strLogo,strBadge} = league;
     return (
         <div className="col-md-4">
             <div class="card mt-3">
-                <img src={strFanart1} class="card-img-top" alt="..."/>
+                <img src={strFanart1 || strFanart2 || strBanner || strLogo || strBadge} style={{height: '200px'}} class="card-img-top" alt="..."/>
                 <div class="card-body">
                     <h5 class="card-title">{strLeague}</h5>
                     <p class="card-text" style={{height: '100px',overflow: 'hidden'}}>{strDescriptionEN}</p>
