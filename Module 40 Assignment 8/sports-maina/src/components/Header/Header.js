82a7b89986ca4e3a25277775441863aed260c5e0
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-25 17:18:37 +0600

    styling

--- Module 40 Assignment 8/sports-maina/src/components/Header/Header.js
+++ Module 40 Assignment 8/sports-maina/src/components/Header/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
+        <div style={{height: '200px',alignItems: 'center',justifyContent: 'center',display: 'flex'}}>
+            <img style={{width: '70%',height: '100%',margin: 'auto'}} className="img-fluid" src="https://play-lh.googleusercontent.com/DEX2nfpOsE6b8rWD5scwkelM9Cnoapqm-O8L6zb5vr82CgGCFB4kZz-bUbcjZ09HWg=w412-h220-rw" alt="" />
         </div>
     );
 };
